a1b5bf144233a04de8860b7b0321a4299aad79ca
Soham <sohampanse2006@gmail.com>
2026-06-26 08:55:43 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from subprocess import call
+call("cls", shell=True)
+
+"""
+Title : Jarvis Assistant
+Author : Soham Panse
+Date : 26 June, 2026
+Purpose : To make an assistant for repetative tasks
+"""
+
+# Global variables and Constants
+
+# Functions
+
+# Main Function
+if __name__ == "__main__":
+    print("Hello World")
